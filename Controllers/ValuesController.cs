b39e9c9b1b944590c283b94d1c0205024cb1c9b0
Arkadiusz Wawrzyniak <arekwaw21@gmail.com>
2021-07-18 14:20:58 +0200

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Tinder1.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+            [HttpGet]
+            public ActionResult<IEnumerable<string>> Get(){
+                return new string[] {"value1", "value2"};
+            }
+
+            [HttpGet("{id}")]
+            public ActionResult<string> Get(int id)
+            {
+                return "Value";
+            }
+
+            [HttpPost]
+            public void Post([FromBody] string value){
+
+            }
+
+            [HttpPut("{id}")]
+            public void put(int id){
+                
+            } 
+
+            [HttpDelete("{id}")]
+            public void delete(int id){
+
+            }
+    }
+}
